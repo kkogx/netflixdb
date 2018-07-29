d00f27a3289b32d789544fbf9f48fdf9b5727a14
zhentarin <753dfxgh>
2018-07-29 11:22:37 +0000

    footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'jhi-footer',
-    templateUrl: './footer.component.html'
+    templateUrl: './footer.component.html',
+    styleUrls: ['footer.css']
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+    baseUrl: String;
+
+    ngOnInit(): void {
+        this.baseUrl = 'www.netflixdb.pl';
+    }
+}
--- src/main/webapp/app/layouts/index.ts
+++ src/main/webapp/app/layouts/index.ts
@@ -2,6 +2,7 @@ export * from './error/error.component';
 export * from './error/error.route';
 export * from './main/main.component';
 export * from './footer/footer.component';
+export * from './footer/footer.route';
 export * from './navbar/navbar.component';
 export * from './navbar/navbar.route';
 export * from './profiles/page-ribbon.component';
