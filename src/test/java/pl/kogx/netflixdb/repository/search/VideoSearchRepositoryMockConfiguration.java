bf12a98ecf3e46bafa3811c8168ac2e12fe576a4
zhentarin <753dfxgh>
2018-07-29 21:17:37 +0000

    bugfix

--- src/test/java/pl/kogx/netflixdb/repository/search/VideoSearchRepositoryMockConfiguration.java
+++ src/test/java/pl/kogx/netflixdb/repository/search/VideoSearchRepositoryMockConfiguration.java
@@ -1,16 +1,16 @@
-package pl.kogx.netflixdb.repository.search;
-
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * Configure a Mock version of VideoSearchRepository to test the
- * application without starting Elasticsearch.
- */
-@Configuration
-public class VideoSearchRepositoryMockConfiguration {
-
-    @MockBean
-    private VideoSearchRepository mockVideoSearchRepository;
-
-}
+//package pl.kogx.netflixdb.repository.search;
+//
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * Configure a Mock version of VideoSearchRepository to test the
+// * application without starting Elasticsearch.
+// */
+//@Configuration
+//public class VideoSearchRepositoryMockConfiguration {
+//
+//    @MockBean
+//    private VideoSearchRepository mockVideoSearchRepository;
+//
+//}
