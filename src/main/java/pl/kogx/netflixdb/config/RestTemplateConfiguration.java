113f869c4ac526855837817564c9e3f9e58fdf21
zhentarin <753dfxgh>
2018-08-11 13:47:05 +0000

    https proxy

--- src/main/java/pl/kogx/netflixdb/config/RestTemplateConfiguration.java
+++ src/main/java/pl/kogx/netflixdb/config/RestTemplateConfiguration.java
@@ -8,6 +8,8 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +19,13 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfiguration {
 
+    private final Logger log = LoggerFactory.getLogger(RestTemplateConfiguration.class);
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder, ApplicationProperties applicationProperties) {
         ApplicationProperties.NetflixSync.ProxySettings proxySettings = applicationProperties.getNetflixSync().getProxySettings();
         if (proxySettings != null && proxySettings.isEnabled()) {
+            log.info("Proxy enabled host={} port={} user={}", proxySettings.getHost(), proxySettings.getUser(), proxySettings.getPort());
             Credentials credentials = new UsernamePasswordCredentials(proxySettings.getUser(), proxySettings.getPassword());
             CredentialsProvider credsProvider = new BasicCredentialsProvider();
             AuthScope authScope = new AuthScope(proxySettings.getHost(), proxySettings.getPort());
