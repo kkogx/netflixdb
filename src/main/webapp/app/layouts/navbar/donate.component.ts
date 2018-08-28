c23938038100db51c1199f58d0e031a6ab302de6
zhentarin <753dfxgh>
2018-08-28 18:32:03 +0000

    paypal

--- src/main/webapp/app/layouts/navbar/donate.component.ts
+++ src/main/webapp/app/layouts/navbar/donate.component.ts
@@ -18,9 +18,9 @@ import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
         </div>
         <div class="modal-body">
             <ng4-loading-spinner></ng4-loading-spinner>
-            <form ngNoForm action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_parent">
+            <form ngNoForm action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                 <input type="hidden" name="cmd" value="_s-xclick">
-                <input type="hidden" name="hosted_button_id" value="ZSYP4WDE2767U">
+                <input type="hidden" name="hosted_button_id" value="AUSN8EHWBSDGS">
                 <input type="image" src="https://www.paypalobjects.com/pl_PL/PL/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – Płać wygodnie i bezpiecznie">
                 <img alt="" border="0" src="https://www.paypalobjects.com/pl_PL/i/scr/pixel.gif" width="1" height="1">
             </form>
