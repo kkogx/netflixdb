b4d84d547e51d74896f94b29fc7f5485a21c9513
zhentarin <zhentarin@gmail.com>
2018-12-23 17:30:32 +0000

    diff svc

--- src/main/java/pl/kogx/netflixdb/NetflixdbApp.java
+++ src/main/java/pl/kogx/netflixdb/NetflixdbApp.java
@@ -16,14 +16,21 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.context.SecurityContextHolder;
 import pl.kogx.netflixdb.config.ApplicationProperties;
 import pl.kogx.netflixdb.config.DefaultProfileUtil;
+import pl.kogx.netflixdb.service.VideoService;
 import pl.kogx.netflixdb.service.fwebsync.FwebSyncService;
 import pl.kogx.netflixdb.service.netflixsync.NetflixSyncService;
 import pl.kogx.netflixdb.service.omdbsync.OmdbSyncService;
+import pl.kogx.netflixdb.service.util.VideoDiffService;
 
 import javax.annotation.PostConstruct;
 import java.net.InetAddress;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @SpringBootApplication
 @EnableAsync
@@ -38,17 +45,23 @@ public class NetflixdbApp implements ApplicationRunner {
 
     private final FwebSyncService fwebSyncService;
 
+    private final VideoDiffService diffService;
+
+    private final VideoService videoService;
+
     private final Environment env;
 
     private boolean scheduled = false;
 
     @Autowired
-    public NetflixdbApp(Environment env, NetflixSyncService netflixSyncService, OmdbSyncService omdbSyncService, FwebSyncService fwebSyncService) {
+    public NetflixdbApp(Environment env, NetflixSyncService netflixSyncService, OmdbSyncService omdbSyncService, FwebSyncService fwebSyncService, VideoDiffService diffService, VideoService videoService) {
         this.env = env;
         this.netflixSyncService = netflixSyncService;
         this.omdbSyncService = omdbSyncService;
         this.fwebSyncService = fwebSyncService;
-     }
+        this.diffService = diffService;
+        this.videoService = videoService;
+    }
 
     /**
      * Initializes netflixdb.
@@ -107,37 +120,70 @@ public class NetflixdbApp implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        log.info("Application was executed with args:");
-        for(String opt: args.getOptionNames()) {
-            log.info("{}={}", opt, args.getOptionValues(opt));
-        }
-
-        for(String opt : args.getOptionNames()) {
-            switch(opt) {
+        log.info("Application was executed with args: " + Arrays.toString(args.getSourceArgs()));
+        for (String opt : args.getOptionNames()) {
+            switch (opt) {
                 case "scheduled": {
                     scheduled = getBooleanOptionValue(args, opt);
                     break;
                 }
                 case "sync": {
-                    netflixSyncService.sync();
-                    fwebSyncService.sync();
-                    omdbSyncService.sync();
+                    doSync(null);
+                    break;
+                }
+                case "export": {
+                    diffService.exportVideos("exported_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".json");
+                    break;
+                }
+                case "diff": {
+                    Long id = getLongOptionValue(args, opt);
+                    doSync(id);
+                    diffService.diff(id);
+                    break;
+                }
+                case "difftop": {
+                    Integer count = getIntOptionValue(args, opt);
+                    List<Long> ids = syncTop(Integer.valueOf(count));
+                    diffService.diffTop(ids);
                     break;
                 }
             }
         }
     }
 
+    private List<Long> syncTop(int count) {
+        log.info("Syncing top count=" + count);
+        List<Long> ids = StreamSupport.stream(videoService.findAll().spliterator(), false)
+            .map(v -> v.getId()).collect(Collectors.toList()).subList(0, count);
+        for (Long id : ids) {
+            doSync(id);
+        }
+        return ids;
+    }
+
+    private void doSync(Long id) {
+        log.info("Syncing id=" + id);
+        netflixSyncService.sync(id);
+        omdbSyncService.sync(id);
+        fwebSyncService.sync(id);
+    }
+
     @Scheduled(fixedDelay = 1000 * 60 * 10 /* 10 minutes */)
     public void syncMoviesScheduled() {
         if (scheduled) {
-            netflixSyncService.sync();
-            fwebSyncService.sync();
-            omdbSyncService.sync();
+            doSync(null);
         }
     }
 
     private boolean getBooleanOptionValue(ApplicationArguments args, String opt) {
-        return args.getOptionValues(opt).isEmpty() ? false : Boolean.valueOf(args.getOptionValues(opt).get(0));
+        return (args.getOptionValues(opt) == null || args.getOptionValues(opt).isEmpty()) ? false : Boolean.valueOf(args.getOptionValues(opt).get(0));
+    }
+
+    private Long getLongOptionValue(ApplicationArguments args, String opt) {
+        return (args.getOptionValues(opt) == null || args.getOptionValues(opt).isEmpty()) ? null : Long.valueOf(args.getOptionValues(opt).get(0));
+    }
+
+    private Integer getIntOptionValue(ApplicationArguments args, String opt) {
+        return (args.getOptionValues(opt) == null || args.getOptionValues(opt).isEmpty()) ? null : Integer.valueOf(args.getOptionValues(opt).get(0));
     }
 }
